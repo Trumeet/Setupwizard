2b2a1ddd3a7aa7eff31541cbbe16787217de6f61
Trumeet <liangyuteng12345@gmail.com>
2018-10-14 19:45:18 -0700

    Import android-9.0.0_r9

---
+++ app/src/main/java/com/android/MainActivity.java
@@ -0,0 +1,12 @@
+package com.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
